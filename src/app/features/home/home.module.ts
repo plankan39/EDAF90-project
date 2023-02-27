2d1631282f113b8d510ce789a7ded33b0d2a1fef
Oscar Peyron <peyronoscar@gmail.com>
2023-02-27 13:32:44 +0100

    Home page done

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,17 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './pages/home/home.component';
-
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./pages/home/home.component";
+import { SharedModule } from "src/app/components/shared/shared.module";
+import { MaterialModule } from "src/app/modules/material.module";
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, MaterialModule],
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { User } from "@angular/fire/auth";
+import {
+  collection,
+  collectionData,
+  doc,
+  docData,
+  DocumentData,
+  Firestore,
+  limit,
+  orderBy,
+  query,
+} from "@angular/fire/firestore";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
+  recipes: DocumentData[] = [];
+  user: DocumentData = {};
+  authUser: User | null = null;
+  ingredients: any[] = [];
+  loadingRecipes: boolean = true;
 
+  constructor(private firestore: Firestore, private auth: AuthService) {
+    this.auth.getUser().subscribe((user) => {
+      this.authUser = user;
+
+      if (user) {
+        const col = collection(this.firestore, "users", user.uid, "recipes");
+        collectionData(query(col), {
+          idField: "id",
+        }).subscribe((recipes) => {
+          this.recipes = recipes;
+          this.loadingRecipes = false;
+        });
+
+        docData(doc(this.firestore, "users", user.uid), {
+          idField: "id",
+        }).subscribe((user: DocumentData) => (this.user = user));
+      }
+    });
+
+    const col = collection(this.firestore, "ingredients");
+
+    collectionData(query(col, orderBy("created", "desc"), limit(10)), {
+      idField: "id",
+    }).subscribe((ingredients) => (this.ingredients = ingredients));
+  }
 }
